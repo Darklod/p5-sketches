47f6fb50c1845610be8701dd3b5708f879a0ca30
lfx98 <lordfighterx@gmail.com>
2017-07-29 17:15:43 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var engine = require('ejs-locals');
+var bodyParser = require('body-parser');
+var helpers = require('express-helpers')
+var fs = require('fs');
+var app = express();
+
+helpers(app);
+
+app.engine('ejs',engine);
+app.set('view engine', 'ejs');
+app.use(express.static('static'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    fs.readdir('static/projects', (err, files) => {
+        res.render('pages/index', { list: files || [] });
+    })
+})
+
+app.get('/projects/:filename', (req, res) => {
+    res.render('pages/project');
+})
+
+app.listen(3000);
